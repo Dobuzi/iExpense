b3c754e7f23210658a8ad1b11fe606a323713368
Dobuzi <kimjw7981@gmail.com>
2020-10-22 22:26:29 +0900

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by 김종원 on 2020/10/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
